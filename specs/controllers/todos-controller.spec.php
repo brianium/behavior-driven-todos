230f2be67d173b66859c524f2ddee85d76e3591e
Brian Scaturro <scaturrob@gmail.com>
2015-03-28 13:07:22 -0400

    what is our behavior?

--- specs/controllers/todos-controller.spec.php
+++ specs/controllers/todos-controller.spec.php
@@ -1,6 +1,21 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+
 describe('TodosController', function () {
+
+    beforeEach(function () {
+        $this->controller = new TodosController();
+    });
+
     describe('->create()', function () {
-        it('should insert a todo');
+
+        it('should insert a todo', function () {
+            $request = Request::create('/todos', 'POST', [], [], [], [], json_encode([
+                'label' => 'Get groceries'
+            ]));
+
+            $this->controller->create($request);
+            // what is our behavior?
+        });
     });
 });
